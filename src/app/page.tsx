ad31689ad8cab74a85df5c73dfd9adffeb415afb
tjpeyton <tim.peyton@protonmail.com>
2024-11-15 17:49:33 -0500

    interview over

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,44 @@
-import Image from "next/image";
+
+'use client'
+
+import { useState } from "react";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+export default function Home() {
+  const [numRolls, setNumRolls] = useState(0)
+  const [sevens, setSevens] = useState(0)
+
+  const rollDice = () => {
+    let sevens = 0
+    for (let i = 0; i < numRolls; i++) {
+      // should be 1 - 6
+      const roll1 = Math.floor(Math.random() * 6) + 1
+      const roll2 = Math.floor(Math.random() * 6) + 1
+  
+      if(roll1 + roll2 === 7) sevens++
+    }
+    setSevens(sevens)
+  }
+
+  return (
+    <>
+    <div className={styles.container}>
+    <form onSubmit={(event) => {event.preventDefault(); rollDice()}} >
+        <label>
+          Number of Roles:
+          <input onChange={(event) => {setNumRolls(Number(event?.target.value))}} type="number"></input>
+        </label>
+        <button type="submit">Roll</button>
+      </form>
+      <div>
+        { numRolls > 0 ? 
+          ((sevens / numRolls) * 100).toFixed(2)
+          : <></>
+        }
+      </div>
     </div>
-  );
+    </>
+  )
 }
